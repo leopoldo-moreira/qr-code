34265a546b87ae98bc933a963880d200eef31069
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-02-13 16:58:06 -0300

    Finish project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
+import qrCodeImage from './img/image-qr-code.png'
+
 function App() {
   return (
     <div className="App">
-     
+      <div className="qrContainer">
+        <img src={qrCodeImage}></img>
+      </div>
+      <h1>Improve your front-end skills by building projects</h1>
+      <p>Scan the QR code to visit Frontend Mentor and take
+         your coding skills to the next level</p>
     </div>
   );
 }
