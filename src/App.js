20d447507ed67d1610342f4bd72ce781b4f5187f
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-02-13 17:57:57 -0300

    Alt to image

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="qrContainer">
-        <img src={qrCodeImage}></img>
+        <img alt="qr-code image" src={qrCodeImage}></img>
       </div>
       <h1>Improve your front-end skills by building projects</h1>
       <p>Scan the QR code to visit Frontend Mentor and take
